8c7017d98338d421b39e36d84e5c586521966f39
deano98 <deano_98@hotmail.com>
2021-07-05 18:55:18 +0000

    Add basic JS structure

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function pickAction() {
+
+}
+
+function checkWinnerRock() {
+
+}
+
+function checkWinnerPaper() {
+
+}
+
+function checkWinnerScissors() {
+
+}
+
+function checkWinnerLizard() {
+
+}
+
+function checkWinnerSpock() {
+
+}
+
+function incrementScore() {
+
+}
+
