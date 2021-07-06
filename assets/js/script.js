492d5060741eae2435ba7c455155b35c0e8aebf1
deano98 <deano_98@hotmail.com>
2021-07-06 17:22:51 +0000

    Add runGame and pickAction functions to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,50 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            let actionChoice = this.getAttribute("data-type");
-            alert(`You clicked ${actionChoice}`)
+            let playerChoice = this.getAttribute("data-type");
+            alert(`You clicked ${playerChoice}`);
+            runGame(playerChoice);
         })
     }
 })
 
-function runGame() {
+function runGame(playerChoice) {
 
+    let num1 = Math.floor(Math.random() * 5) + 1;
+    console.log(num1);
+
+    let computerChoice = "";
+
+    if (num1 === 1) {
+        computerChoice = "computerRock";
+    } else if (num1 === 2) {
+        computerChoice = "computerPaper";
+    } else if (num1 === 3) {
+        computerChoice = "computerScissors";
+    } else if (num1 === 4) {
+        computerChoice = "computerLizard";
+    } else if (num1 === 5) {
+        computerChoice = "computerSpock";
+    }
+    pickAction(playerChoice, computerChoice)
 }
 
-function pickAction() {
-
+function pickAction(computerChoice) {
+    if (playerChoice === "rock") {
+        checkWinnerRock(computerChoice);
+    } else if (playerChoice === "paper") {
+        checkWinnerPaper(computerChoice);
+    } else if (playerChoice === "scissors") {
+        checkWinnerScissors(computerChoice);
+    } else if (playerChoice === "lizard") {
+        checkWinnerLizard(computerChoice); 
+    } else if (playerChoice === "spock") {
+        checkWinnerSpock(computerChoice); 
+    }
 }
 
-function checkWinnerRock() {
-
+function checkWinnerRock(computerChoice) {
+    if computerChoice
 }
 
 function checkWinnerPaper() {
